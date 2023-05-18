ccd4c9c6ce04fa5cbbcea7de9f0a5646d81f629c
Meryembm <mahnaouimeryem920@gmail.com>
2023-05-18 01:55:14 -0700

    task0

--- main.h
+++ main.h
@@ -1,19 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *a);
-char *cap_string(char *a);
-char *leet(char *a);
-char *rot13(char *a);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
 
